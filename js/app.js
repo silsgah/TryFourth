cf0e812fcc74eb434e297259e35ce49a8948e1ea
silsgah <silasgah@yahoo.co.uk>
2014-08-09 12:53:23 -0700

    fouth
    description

---
+++ js/app.js
@@ -0,0 +1,55 @@
+﻿window.kendoMobileApplication = new kendo.mobile.Application(document.body, { transition: "slide", layout: "tabstrip-layout", skin: "flat" });
+var onDeviceReady = function () {
+    //window.plugins.socialsharing.share("message", "subject");
+  
+   
+};
+document.addEventListener('deviceready', onDeviceReady, false);
+
+
+function closeModalViewAdd() {
+    $("#modalview-add").kendoMobileModalView("close");
+}
+function reply() {
+    window.location.href = '#tabstrip-mainstart';
+}
+function sharefacebook() {
+    window.plugins.socialsharing.shareViaFacebook('Message via Facebook', null, null, function () { console.log('share ok') }, function (errormsg) { alert(errormsg) })
+
+}
+function showMap() {
+    navigator.geolocation.getCurrentPosition(onSuccessShowMap,onErrorShowMap);
+}
+function onErrorShowMap(error) {
+    alert(error);
+}
+function onSuccessShowMap(position) {
+
+    //var latlng = new google.maps.LatLng(
+    //position.coords.latitude,
+    //position.coords.longitude);
+    var latlng = new google.maps.LatLng(36.9759254, -121.9533386);
+   
+    var mapOptions = {
+        sensor: true,
+        center: latlng,
+        panControl: false,
+        zoomControl: true,
+        zoom: 16,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        streetViewControl: false,
+        mapTypeControl: true,
+    };
+
+    var map = new google.maps.Map(
+    document.getElementById('map_canvas'),
+    mapOptions
+    );
+
+    var marker = new google.maps.Marker({
+        position: latlng,
+        map: map
+    });
+    console.log(marker);
+    console.log("map rendering");
+}
---
+++ js/cordova-plugins.js
@@ -0,0 +1,87 @@
+#!/usr/bin/env node
+
+(function() {
+    'use strict';
+    var clc = require('cli-color'),
+        exit = require('exit'),
+        fs = require('fs'),
+        nopt = require('nopt'),
+        path = require('path'),
+        // prompt = require( 'prompt' ),
+        updateNotifier = require('update-notifier'),
+        knownOpts = {
+            'platforms': String,
+            'repo': String
+        },
+        shortHands = {
+            'p': ['--platforms'],
+            'r': ['--repo']
+        },
+        parsed = nopt(knownOpts, shortHands, process.argv, 2),
+        plugreg = require('../scripts/plugreg-com'),
+        registrycordova = require('../scripts/registry-cordova-io'),
+        args = process.argv.splice(2);
+
+    if (args.indexOf('--no-update-notifier') === -1) {
+        // Checks for available update and returns an instance
+        var notifier = updateNotifier( { packagePath: '../package.json', updateCheckInterval: 1000 * 60 * 60 * 24 } );
+        if (notifier.update) {
+            // Notify using the built-in convenience method
+            notifier.notify(true);
+        }
+    }
+
+    if (args.length === 0) {
+        console.log( '' );
+        console.log( 'cordova-plugins [options] command [term]');
+        console.log( '' );
+        console.log( '   commands:');
+        console.log( '       list       - display all available plugins');
+        console.log( '       search     - search plugins for a specfic term/phrase ');
+        console.log( '' );
+        console.log( '   options:');
+        console.log( '       -p         - comma delimited (no spaces) platforms to filter by.');
+        console.log('                    Valid platforms are: ' + clc.redBright( 'ios android blackberry10 wp7 wp8 firefoxos' ) + '.' );
+        console.log('                    Default is all.');
+        console.log( '       -r         - the url/name of the repo to use.');
+        console.log('                    Valid urls are: '+ clc.yellowBright( 'http://plugreg.com') + ' or use shortform ' + clc.yellowBright( 'plugreg' ) );
+        console.log('                    and ' + clc.yellowBright('http://registry.cordova.io') + ' or use shortform ' + clc.yellowBright( 'cordova' ) + '.');
+        console.log('                    Default is http://registry-cordova-io.cordova.io');
+        console.log( '' );
+        console.log( '   term           - term to use when using the `search` command.');
+        console.log('                    Wrap the term in quotes if there are spaces.');
+        console.log( '\nexample: cordova-plugins -p ios,android list');
+        console.log( '' );
+    }
+    else {
+        var platforms = (parsed.platforms ? parsed.platforms.split(',') : []),
+            repo = parsed.repo,
+            command = parsed.argv.remain[0] || 'list',
+            plugin = parsed.argv.remain[1],
+            userconfig = null;
+
+            // userconfig = require(process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME']+'/.cordova-plugins');
+        try {
+            userconfig = JSON.parse(fs.readFileSync(process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME']+'/.cordova-plugins', 'utf8' ));
+            repo = repo || (userconfig && userconfig.repo);
+        }
+        catch(e) {
+            console.log( 'Cannot find or parse config file.  Please make sure it exists and is valid json. Using Defaults.\n');
+        }
+
+        switch (command.toLowerCase()) {
+        case 'list':
+        case 'search':
+            if( repo && ( repo.toLowerCase().indexOf( 'plugreg' ) !== -1 ) ) {
+                plugreg.fetch(plugin,platforms,userconfig);
+            }
+            else {
+                registrycordova.fetch(plugin,platforms,userconfig);
+            }
+            break;
+        // case 'get':
+            // break;
+        default:
+        }
+    }
+})();
