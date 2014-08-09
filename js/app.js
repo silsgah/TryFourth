3589fef86909feae7b2a2416554d85cc054752b9
silsgah <silasgah@yahoo.co.uk>
2014-08-09 16:36:26 -0700

    Fifth
    changes description

--- js/app.js
+++ js/app.js
@@ -14,9 +14,30 @@ function reply() {
     window.location.href = '#tabstrip-mainstart';
 }
 function sharefacebook() {
-    window.plugins.socialsharing.shareViaFacebook('Message via Facebook', null, null, function () { console.log('share ok') }, function (errormsg) { alert(errormsg) })
-
+    window.plugins.socialsharing.shareViaFacebook('Message via Facebook', null, null, function () { console.log('share ok') }, function (errormsg) { alert(errormsg) });
 }
+function sharetwitter() {
+    window.plugins.socialsharing.shareViaTwitter('Message via Twitter');
+}
+function sendsms() {
+    window.plugins.socialsharing.shareViaSMS('My cool message', '233268468627', function (msg)
+    { console.log('ok: ' + msg) },
+    function (msg)
+    { alert('error: ' + msg) })
+}
+function sharemail() {
+    window.plugins.socialsharing.shareViaEmail(
+   'Message',
+   'Subject',
+   ['to@person1.com', 'to@person2.com'], // TO: must be null or an array
+   ['cc@person1.com'], // CC: must be null or an array
+   null, // BCC: must be null or an array
+   ['https://www.google.nl/images/srpr/logo4w.png', 'www/localimage.png'], // FILES: can be null, a string, or an array
+   onSuccess, // called when sharing worked, but also when the user cancelled sharing via email (I've found no way to detect the difference)
+   onError // called when sh*t hits the fan
+ );
+}
+
 function showMap() {
     navigator.geolocation.getCurrentPosition(onSuccessShowMap,onErrorShowMap);
 }
